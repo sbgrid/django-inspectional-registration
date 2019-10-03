7f13aad21909b0e321257d6df45349008935c60e
Henry Mehta <hjsmehta@gmail.com>
2019-10-03 10:30:13 +0100

    Update 0001_initial.py

--- src/registration/migrations/0001_initial.py
+++ src/registration/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('_status', models.CharField(editable=False, default='untreated', max_length=10, choices=[('untreated', 'Untreated yet'), ('accepted', 'Registration has accepted'), ('rejected', 'Registration has rejected')], verbose_name='status', db_column='status')),
                 ('activation_key', models.CharField(null=True, default=None, verbose_name='activation key', max_length=40, editable=False)),
-                ('user', models.OneToOneField(editable=False, related_name='registration_profile', verbose_name='user', to=settings.AUTH_USER_MODEL)),
+                ('user', models.OneToOneField(editable=False, related_name='registration_profile', verbose_name='user', to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
             ],
             options={
                 'permissions': (('accept_registration', 'Can accept registration'), ('reject_registration', 'Can reject registration'), ('activate_user', 'Can activate user in admin site')),
